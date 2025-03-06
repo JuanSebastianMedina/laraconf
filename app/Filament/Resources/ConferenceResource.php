4f54308c56e5d8a583020e4bf539c412f29a844d
Juanse <jmedinapineros@gmail.com>
2025-03-06 16:40:56 -0500

    04. Casillas de verificación

--- app/Filament/Resources/ConferenceResource.php
+++ app/Filament/Resources/ConferenceResource.php
@@ -6,6 +6,7 @@ use Filament\Forms;
 use Filament\Tables;
 use App\Enums\Region;
 use App\Models\Venue;
+use App\Models\Speaker;
 use Filament\Forms\Form;
 use App\Models\Conference;
 use Filament\Tables\Table;
@@ -58,7 +59,13 @@ class ConferenceResource extends Resource
                     ->editOptionForm(Venue::getForm())
                     ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Forms\Get $get){
                         return $query->where('region', $get('region'));
-                    })
+                    }),
+                Forms\Components\CheckboxList::make('speakers')
+                    ->relationship('speakers', 'name')
+                    ->options(
+                        Speaker::all()->pluck('name', 'id')
+                    )
+                    ->required(),
             ]);
     }
 
--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Filament\Forms\Components\Textarea;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\CheckboxList;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Speaker extends Model
@@ -21,10 +24,51 @@ class Speaker extends Model
     // The attributes that should be cast to native types.
     protected $casts = [
         'id' => 'integer',
+        'qualifications' => 'array',
     ];
 
     public function conferences(): BelongsToMany
     {
         return $this->belongsToMany(Conference::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('email')
+                ->email()
+                ->required()
+                ->maxLength(255),
+            Textarea::make('bio')
+                ->required()
+                ->columnSpanFull(),
+            TextInput::make('twitter_handle')
+                ->required()
+                ->maxLength(255),
+            CheckboxList::make('qualifications')
+                ->columnSpanFull()
+                ->searchable()
+                ->bulkToggleable()
+                ->options([
+                    'leader' => 'Liderazgo',
+                    'charisma' => 'Carisma',
+                    'humanitarian' => 'Trabajo humanitario',
+                    'contributor' => 'contribuyente',
+                    'influencer' => 'Influencer',
+                    'open_source' => 'Open Source'
+                ])
+                ->descriptions([
+                    'leader' => 'Descata como lider',
+                    'charisma' => 'Destaca por su carisma',
+                    'humanitarian' => 'Reconocido por su trabajo humanitario',
+                    'contributor' => 'Buen contribuyente',
+                    'influencer' => 'Influencer de Youtube',
+                    'open_source' => 'Activista',
+                ])
+                ->columns(3)
+        ];
     }
 }
